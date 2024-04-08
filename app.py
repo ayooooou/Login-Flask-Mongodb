f6dc6867f109bc7c6a30e6d965aa5b7fd14a2d28
Ayou .w <heyyutop5@gmail.com>
2024-04-08 21:39:58 +0800

    新增防止重複帳號 及 錯誤訊息提示

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app.secret_key = 'hello,wood'
 mongo = PyMongo(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+class User(UserMixin):
+    pass
 
 @app.route("/")
 def home_page():
@@ -16,50 +17,44 @@ def home_page():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_page():
+    error_message = ""
     if request.method == 'POST':
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
-        user = {'email': email, 'username': username, 'password': password}
-        mongo.db.user.insert_one(user)
-        return redirect(url_for('home_page'))
-    return render_template("register.html")
+        existing_email = mongo.db.user.find_one({'email': email})
+        existing_username = mongo.db.user.find_one({'username': username})
+        if existing_email:
+            error_message += "\nEmail already exists!"
+        if existing_username:
+            error_message += "\nUsername already exists!"
+        if not existing_email and not existing_username:
+            user = {'email': email, 'username': username, 'password': password}
+            mongo.db.user.insert_one(user)
+            return redirect(url_for('login_page'))
+    return render_template("register.html",error_message=error_message)
 
 @app.route("/login", methods=['GET','POST'])
 def login_page():
+    error_message = ""
     if request.method == 'POST':
+        if current_user.is_active: 
+            return 'has logined'
         username = request.form['username']
         password = request.form['password']
         user_find = mongo.db.user.find_one({'username': username})
         if user_find and user_find['password']==password:
-            #  實作User類別  
-            user = User()  
-            #  設置id就是email  
-            user.id = username
-            #  這邊，透過login_user來記錄user_id，如下了解程式碼的login_user說明。  
-            login_user(user)  
-            return redirect(url_for('home_page'))
-        return '登入失敗'
+            user = User()  #  實作User類別
+            user.id = username #為了讓 Flask-Login 知道哪個用戶已經登入
+            login_user(user)   
+            return redirect(url_for('protected_page'))
+        elif user_find:
+            error_message = '\nWrong password'
+        else:
+            error_message = "\nUnknown username"
+        return render_template("login.html",error_message=error_message)
     return render_template("login.html")
 
-
-@app.route('/submit/login', methods=['POST'])
-def submit_login():
-    username = request.form['username']
-    password = request.form['password']
-    user_find = mongo.db.user.find_one({'username': username})
-    if user_find and user_find['password']==password:
-        #  實作User類別  
-        user = User()  
-        #  設置id就是email  
-        user.id = username
-        #  這邊，透過login_user來記錄user_id，如下了解程式碼的login_user說明。  
-        login_user(user)  
-        return redirect(url_for('home_page'))  
-    return '登入失敗'  
-
-class User(UserMixin):
-    pass
 
 @login_manager.user_loader  
 def user_loader(username):  
@@ -68,7 +63,7 @@ def user_loader(username):
     return user 
 
 @app.route('/logout')  
-def logout():  
+def logout_page():  
     if current_user.is_active: 
         logout_user()  
         return 'Logged out'
@@ -76,15 +71,12 @@ def logout():
         return "you aren't login"
     
 
-#登入狀態
 @app.route('/protected')  
-@login_required  
-def protected():  
-    #  current_user確實的取得了登錄狀態
+@login_required  #只能在用戶登錄後訪問
+def protected_page():  
+    #  current_user取得登錄狀態
     if current_user.is_active:  
         return 'Logged in as: ' + current_user.id + 'Login is_active:True'
-    else:
-        return "you aren't login"
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
